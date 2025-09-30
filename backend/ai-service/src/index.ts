375952a1444c498ee8631e196b3a23718577dbcf
Arjun p s <hai.arjunps@gmail.com>
2025-10-01 00:33:25 +0530

    auth service,reservation service,restaurent service, ai service created

---
+++ backend/ai-service/src/index.ts
@@ -0,0 +1,20 @@
+import express, { Express } from 'express';
+import type { Request, Response } from 'express';
+import cors from 'cors';
+
+const app: Express = express();
+const port = 3003;
+
+app.use(
+  cors({
+    origin: "http://localhost:5173",
+    credentials: true,
+  })
+);
+app.get('/', (req: Request, res: Response) => {
+  res.send('Hello World with ai-service !');
+});
+
+app.listen(port, () => {
+  console.log(`Server is running on http://localhost:${port}`);
+});
---
+++ backend/restaurant-service/dist/index.js
@@ -0,0 +1,20 @@
+"use strict";
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
+Object.defineProperty(exports, "__esModule", { value: true });
+const express_1 = __importDefault(require("express"));
+const cors_1 = __importDefault(require("cors"));
+const app = (0, express_1.default)();
+const port = 3002;
+app.use((0, cors_1.default)({
+    origin: "http://localhost:5173",
+    credentials: true,
+}));
+app.get('/', (req, res) => {
+    res.send('Hello World with restaurent-service !');
+});
+app.listen(port, () => {
+    console.log(`Server is running on http://localhost:${port}`);
+});
+//# sourceMappingURL=index.js.map
